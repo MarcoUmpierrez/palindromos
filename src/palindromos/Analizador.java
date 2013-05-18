132e59d7e30c9b22d94d0baab175f28b4ce4f61c
usuario <usuario@10.22.146.38>
2013-05-18 12:26:12 +0100

    Añadimos método esPalindromoMayorDeUnaCifra

--- src/palindromos/Analizador.java
+++ src/palindromos/Analizador.java
@@ -10,17 +10,32 @@ package palindromos;
  */
 class Analizador {
 
-    boolean esPalindromo(int i) {
-        if(PalindromoUnaCifra(i))
+    boolean esPalindromo(int ValorEvaluado) {
+        if(esPalindromoUnaCifra(ValorEvaluado))
+            return true;
+        if(esPalindromoMayorDeUnaCifra(ValorEvaluado))
             return true;
         return false;
     }
 
-    private boolean PalindromoUnaCifra(int i) {
-        if(i<10 && i>0)
+    private boolean esPalindromoUnaCifra(int ValorEvaluado) {
+        if(ValorEvaluado<10 && ValorEvaluado>0)
             return true;
         else
             return false;
+    }
+
+    private boolean esPalindromoMayorDeUnaCifra(int ValorEvaluado) {
+        String PalindromoConvertidoAString;
+        int LongitudPalindromo;
+        PalindromoConvertidoAString = String.valueOf(ValorEvaluado);        
+        LongitudPalindromo = PalindromoConvertidoAString.length();        
+        for(int i=0;i<(LongitudPalindromo/2);i++)
+        {   
+              if(PalindromoConvertidoAString.charAt(i) != PalindromoConvertidoAString.charAt(LongitudPalindromo-(i+1)))
+                return false;
+        }
+        return true;
     }
     
 }
