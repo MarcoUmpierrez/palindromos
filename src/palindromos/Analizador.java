fec3349e934acf6d648a0ac2746865544362408e
usuario <usuario@10.22.146.38>
2013-05-18 11:11:32 +0100

    Añadimos método PalindromoUnaCifra

--- src/palindromos/Analizador.java
+++ src/palindromos/Analizador.java
@@ -9,5 +9,18 @@ package palindromos;
  * @author usuario
  */
 class Analizador {
+
+    boolean esPalindromo(int i) {
+        if(PalindromoUnaCifra(i))
+            return true;
+        return false;
+    }
+
+    private boolean PalindromoUnaCifra(int i) {
+        if(i<10 && i>0)
+            return true;
+        else
+            return false;
+    }
     
 }
